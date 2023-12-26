6e4108bb52f4a21e72a69015dab7f02d52829d22
Kote <144833957+YakinFsilva@users.noreply.github.com>
2023-12-26 13:24:31 -0300

    Add files via upload

---
+++ header.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.css'; 
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Lista de Alunos</Link>
+          </li>
+          <li>
+            <Link to="/create">Criar/Atualizar Aluno</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
